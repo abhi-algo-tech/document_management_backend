b4261b415c9901cb2078a007dfd38d3e6bd3455d
abhi-algo-tech <abhishek@millicent.in>
2024-08-04 23:34:31 +0530

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allows all origins
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
